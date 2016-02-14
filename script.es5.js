d653cb5bfd065b9487f80af02d2e7e55ac8737eb
Paul Kögel <paul@railslove.com>
2016-02-14 15:46:46 +0100

    update

--- script.es5.js
+++ script.es5.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -56,3 +58,11 @@ var Bar = function Bar() {
 Bar.myClassProp = 'ERM';
 
 var bar = new Bar();
+
+var youngPeter = { first: 'Peter', last: 'Parker', age: 20 };
+
+var oldPeter = _extends({}, youngPeter, { age: 70 });
+var stillPeter = _extends({ first: 'John' }, youngPeter);
+
+var ids = [1, 2, 3, 4];
+var newIds = [77].concat(ids, [99]); // [77, 1, 2, 3, 4, 99]
--- script.es6.js
+++ script.es6.js
@@ -1,10 +1,17 @@
 const PI = 3.14159;
 
+
+
+
 class Foo {
   render() {
     return '<h1>Some Headline</h1>';
   }
 }
+
+
+
+
 
 class Alert {
 
@@ -22,6 +29,8 @@ let myAlert = new Alert;
 setTimeout(myAlert.jim, 500);
 
 
+
+
 class Bar {
   myProp = 42;
   static myClassProp = 'ERM';
@@ -32,3 +41,13 @@ class Bar {
 }
 
 let bar = new Bar;
+
+
+let youngPeter = { first: 'Peter', last: 'Parker', age: 20 };
+
+let oldPeter = { ...youngPeter, age: 70 };
+let stillPeter = { first: 'John', ...youngPeter };
+
+
+let ids = [1, 2, 3, 4];
+let newIds = [77, ...ids, 99]; // [77, 1, 2, 3, 4, 99]
